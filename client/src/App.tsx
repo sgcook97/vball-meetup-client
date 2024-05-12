12ed7339ec15ea449cea158e994e1bb3231b702b
Sam Cook <opp11@txstate.edu>
2024-05-12 18:15:28 -0500

    created vite-react app

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="mt-10 flex flex-col justify-center items-center">
+        <p>Hello World</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
